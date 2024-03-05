924a9eb9f426ac6fefc63a398da0735f3aa150d7
mokhatiri <mohamed.khatiri2006@gmail.com>
2024-03-05 17:30:11 +0100

    changes over this project

--- C_code/game_record.c
+++ C_code/game_record.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <ctype.h>  // Include ctype.h for isdigit function
 
 int calPoints(char **operations, int operationsSize) {
-    int *record[1000];
+    int *record = malloc(sizeof(int)*1000);
+    assert(record);
     int pointer = 0;
     int sum = 0;
 
@@ -14,58 +16,40 @@ int calPoints(char **operations, int operationsSize) {
 
         if (isdigit(op) || (op == '-' && isdigit(*(c + 1))))
         {
-            record[pointer] = malloc(sizeof(int));
-            // just a good thing to do
-            if (record[pointer] == NULL) {
-                printf("Memory allocation failed");
-                exit(EXIT_FAILURE);
-            }
             
-            *record[pointer] = atoi(c + (op == '-' ? 1 : 0));
-            
+            record[pointer] = atoi(c + (op == '-' ? 1 : 0));
             if (op == '-') {
-                *record[pointer] *= -1;
+                record[pointer] *= -1;
             }
-            
             pointer++;
         }
 
         else if (op == 'D')
         {
-            record[pointer] = malloc(sizeof(int));
-            if (record[pointer] == NULL) {
-                printf("Memory allocation failed");
-                exit(EXIT_FAILURE);
-            }
-            *record[pointer] = *record[pointer - 1] * 2;
+            
+            record[pointer] = record[pointer - 1] * 2;
             pointer++;
         }
 
         else if (op == '+')
         {
-            record[pointer] = malloc(sizeof(int));
-            if (record[pointer] == NULL) {
-                printf("Memory allocation failed");
-                exit(EXIT_FAILURE);
-            }
-            *record[pointer] = *record[pointer - 1] + *record[pointer - 2];
+            record[pointer] = record[pointer - 1] + record[pointer - 2];
             pointer++;
         }
 
-        else
+        else if (op == 'C')
         {
-            free(record[pointer - 1]);
             pointer--;
         }
     }
-
+    printf("the record is : [ ");
     for (int k = 0; k < pointer; k++)
     {
-        sum += *record[k];
-        printf("%d\n",sum);
-        free(record[k]);
+        sum += record[k];
+        printf("%d,",record[k]);
     }
-
+    printf("]\n");
+    free(record);
     return sum;
 }
 
